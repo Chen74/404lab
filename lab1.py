7068d8d0cf56f4f929b8963798bef491dfacb14e
Chen74 <54929297+Chen74@users.noreply.github.com>
2022-01-13 22:38:38 -0700

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -3,3 +3,5 @@ version = requests.__version__
 print(version)
 
 print(requests.get("http://google.com/").text)
+
+print(requests.get("https://raw.githubusercontent.com/Chen74/404lab/main/lab1.py").text)
